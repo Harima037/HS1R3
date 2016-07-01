a3ab4e8f295982bc686df9e076a4bbec63119f3d
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2016-07-01 10:08:47 -0500

    Correción al obtener la desripcion de un mes.

--- app/SSA/Utilerias/Util.php
+++ app/SSA/Utilerias/Util.php
@@ -99,8 +99,8 @@ class Util
 		if(!$mes){
 			$mes = date('n');
 		}
-		$meses = array(1 => 'Enero', 2 => 'Febrero', 3 => 'Marzo', 4 => 'Abril', 5 => 'Mayo', 6 => 'Junio', 
-					   7 => 'Julio', 8 => 'Agosto', 9 => 'Septiembre', 10 => 'Octubre', 11 => 'Noviembre', 12 => 'Diciembre');
+		$mes = intval($mes);
+		$meses = array(1 => 'Enero', 2 => 'Febrero', 3 => 'Marzo', 4 => 'Abril', 5 => 'Mayo', 6 => 'Junio', 7 => 'Julio', 8 => 'Agosto', 9 => 'Septiembre', 10 => 'Octubre', 11 => 'Noviembre', 12 => 'Diciembre');
 		return $meses[$mes];
 	}
 
